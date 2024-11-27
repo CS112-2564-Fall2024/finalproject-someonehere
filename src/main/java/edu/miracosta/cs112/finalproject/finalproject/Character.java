c4c5da7a288514bdbe0a6d60fc2ee54b583c00f0
chase <chasematthewraines@gmail.com>
2024-11-27 11:26:12 -0800

    Added Character.java, not sure if this should still be the main or not.

---
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Character.java
@@ -0,0 +1,45 @@
+package edu.miracosta.cs112.finalproject.finalproject;
+
+public class Character {
+    private Rect image;
+    private Movement move;
+    protected int HP;
+
+    /**
+     * Create a Character from a subclass */
+    protected Character() {
+    }
+
+    // Getters
+    public Rect getImage () {
+        return this.image;
+    }
+    public Movement getMove () {
+        return this.move;
+    }
+    public Movement getDash () {
+        return this.dash;
+    }
+    public int getHP () {
+        return this.HP;
+    }
+
+    // Setters
+    public void setImage (Rect image) {
+        this.image = image;
+    }
+    public void setMove (Movement move) {
+        this.move = move;
+    }
+    public void setDash (Movement dash) {
+        this.dash = dash;
+    }
+    public void setHP (int HP) {
+        if (HP >= 0)
+            this.HP = HP;
+    }
+
+    public Movement useDash(Movement move) {
+        this.move = move * 2;
+    }
+}
