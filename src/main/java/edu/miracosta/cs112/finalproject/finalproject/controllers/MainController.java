302b864c6fb8732478d52917a621ad8f36a948b4
Chase Raines <chasematthewraines@gmail.com>
2024-11-27 14:57:50 -0800

    MainController v0.8?

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/MainController.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/controllers/MainController.java
@@ -1,5 +1,8 @@
 package edu.miracosta.cs112.finalproject.finalproject.controllers;
 
+import edu.miracosta.cs112.finalproject.finalproject.GameObject;
+import edu.miracosta.cs112.finalproject.finalproject.Player;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
@@ -19,6 +22,10 @@ public class MainController {
     @FXML
     private Rectangle ground1;
     @FXML
+    private Rectangle ground2;
+    @FXML
+    private Rectangle ground3;
+    @FXML
     private Rectangle box1;
     @FXML
     private Rectangle box2;
@@ -29,36 +36,58 @@ public class MainController {
     @FXML
     private Rectangle box5;
     @FXML
+    private Rectangle box6;
+    @FXML
+    private Rectangle box7;
+    @FXML
+    private Rectangle boxMove1;
+    @FXML
+    private Rectangle boxFinish1;
+    @FXML
+    private Rectangle boxFinish2;
+    @FXML
+    private Rectangle boxFinish3;
+    @FXML
+    private Rectangle boxFinish4;
+    @FXML
+    private Rectangle boxFinish5;
+    @FXML
+    private Rectangle boxFinish6;
+    @FXML
+    private Rectangle boxFinish7;
+    @FXML
+    private Rectangle boxFinish8;
+    @FXML
     private Polygon spike1;
     @FXML
     private Polygon spike2;
     @FXML
-    private Rectangle playerRect;
-    //private Player player;
+    private Rectangle rectPlayer;
+    private Player player;
 
-   // ArrayList<GameObject> objectList = new ArrayList<>();
+    ArrayList<GameObject> objectList = new ArrayList<>();
 
     @FXML
+    private void initialize() {
+        objectList.add(new GameObject(ground1));
+        objectList.add(new GameObject(ground2));
+        objectList.add(new GameObject(ground3));
 
+        player = new Player(rectPlayer);
+        objectList.add(player);
+
+        AnimationTimer timer = (l) -> {
+            for (GameObject o : objectList)
+                o.update();
+        };
+        timer.start();
+    }
 
     protected void handleKeyPressed(KeyEvent e) {
         switch (e.getCode()) {
-            case UP:
-
-                break;
-            case DOWN:
-
-                break;
-            case LEFT:
-
-                break;
-            case RIGHT:
-
-                break;
-            case SPACE: player.jump();
-                break;
-            default:
-
+            case LEFT -> player.moveLeft();
+            case RIGHT -> player.moveRight();
+            case SPACE -> player.jump();
         }
     }
 }
